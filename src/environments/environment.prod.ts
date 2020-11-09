1f647ff7decefce0681a0f252f6baec88b6c3e9b
unknown <mvdcreativo@gmail.com>
2020-11-09 18:32:17 -0300

    cambios en environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,25 @@
 export const environment = {
-  production: true
+  production: true,
+  API: "https://admin.presenza.picaworks.com/api/",
+  APIAuth: "https://admin.presenza.picaworks.com/api/auth/",
+
+
+  ////ROUTES API
+  routesCRUD: {
+    products: "properties",
+    states: "provinces",
+    cities: "cities",
+    countries: "countries",
+    features: "features",
+    municipalities: "municipalities",
+    neighborhoods: "neighborhoods",
+    categories: "property_types",
+    transactionTypes: "transaction_types",
+    publications: "publications",
+    currencies: "currencies",
+    users: "users",
+    expenses: "expenses"
+
+  }
+
 };
